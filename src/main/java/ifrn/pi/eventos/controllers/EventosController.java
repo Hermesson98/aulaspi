fe4f222bcab93a51fe01bd4658706c70c3c9e552
hermeson <hermeson@gmail.com>
2022-10-14 20:12:20 -0300

    Configurando dependências do banco de dados

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -3,14 +3,29 @@ package ifrn.pi.eventos.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ifrn.pi.eventos.models.Evento;
+
 @Controller
 public class EventosController {
 	
 	@RequestMapping("/Eventos/form")
 	public String form() {
+		System.out.println("Chamando o método form: ");
+		
 		return "formEvento";
 		
 	}
+	@RequestMapping("/Eventos/submit")
+	public String submit (Evento h){
+		System.out.println(h.getNome());
+		System.out.println(h.getLocal());
+		System.out.println(h.getHorario());
+		System.out.println(h.getData());
+		
+		return "submit";
+	 
+	 
+ }
 
 
 }
