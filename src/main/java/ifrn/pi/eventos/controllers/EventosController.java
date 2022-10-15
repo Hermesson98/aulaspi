e2fd8cd68d253e5acaa99513aa806469afcbc926
hermeson <hermeson@gmail.com>
2022-10-14 21:11:35 -0300

    Enviando dados do formulario para a aplicação

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,6 +1,7 @@
 package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ifrn.pi.eventos.models.Evento;
@@ -10,22 +11,20 @@ public class EventosController {
 	
 	@RequestMapping("/Eventos/form")
 	public String form() {
-		System.out.println("Chamando o método form: ");
 		
 		return "formEvento";
 		
 	}
-	@RequestMapping("/Eventos/submit")
-	public String submit (Evento h){
-		System.out.println(h.getNome());
-		System.out.println(h.getLocal());
-		System.out.println(h.getHorario());
-		System.out.println(h.getData());
+	
+	@PostMapping("/eventos")
+	public String adicionar(Evento evento) {
 		
-		return "submit";
-	 
-	 
+	System.out.println(evento);
+	
+		
+		return "evento-adicionado";
+		}
+	
+	
  }
 
-
-}
